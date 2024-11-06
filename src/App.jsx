f050909e14c09d436624a7ebf61526398125186d
JonathanChristiano <christianjo.1207@gmail.com>
2024-11-06 10:37:34 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import Navbar from "./Navbar"
+
+function App() {
+
+  return (
+    <>
+      <Navbar/>
+      <div className="min-h-screen w-full relative bg-gradient-to-r
+      from-sky-800 to-gray-900 flex items-center justify-center
+      flex-col text-center gap-10 text-white p-52
+      ">
+        <h1 className="text-7xl">Section 1</h1>
+        <p className="text-3xl">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas ex cupiditate excepturi consectetur libero velit placeat aut ipsam provident voluptate quod quae optio voluptates illo aliquam sint, officia maiores pariatur veritatis nobis, error distinctio? Alias optio ipsum aspernatur, quo ex a nobis sequi fugiat, facilis quos sapiente dolore, eum qui?</p>
+      </div>
+    </>
+  )
+}
+
+export default App
